bf6ad5e198bb3efb5f03fb4c80d3290a262c7fc1
Thomas Lundquist <github@bisonlab.no>
2016-12-12 19:44:37 +0100

    Rewrite the user menu and make the menu thingie easier to handle.

--- Menu/Builder.php
+++ Menu/Builder.php
@@ -15,7 +15,13 @@ class Builder implements ContainerAwareInterface
     public function __construct()
     {
         if (class_exists('AppBundle\Menu\Builder')) {
-            $this->local_builder = new \AppBundle\Menu\Builder();
+            $this->custom_builder = new \AppBundle\Menu\Builder();
+        }
+        if (class_exists('BisonLab\SakonninBundle\Menu\Builder')) {
+            $this->sakonnin_builder = new \BisonLab\SakonninBundle\Menu\Builder();
+        }
+        if (class_exists('BisonLab\CommonBundle\Menu\Builder')) {
+            $this->common_builder = new \BisonLab\CommonBundle\Menu\Builder();
         }
     }
 
@@ -31,12 +37,38 @@ class Builder implements ContainerAwareInterface
             $menu['Admin Stuff']->addChild('User Admin', array('route' => 'user'));
         }
 
-        // Yeah, I'd call this a hack too.
+        $options['menu']      = $menu;
         $options['container'] = $this->container;
         // For local additions to the main menu.
-        if ($this->local_builder
-                && method_exists($this->local_builder, "mainMenu"))
-            return $this->local_builder->mainMenu($factory, $options, $menu);
+        if ($this->custom_builder
+                && method_exists($this->custom_builder, "mainMenu"))
+            $menu = $this->custom_builder->mainMenu($factory, $options);
+        return $menu;
+    }
+
+    public function userMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+
+        // Yeah, I'd call this a hack too.
+        $options['menu']      = $menu;
+        $options['container'] = $this->container;
+
+        $menu = $this->common_builder->userMenu($factory, $options);
+        $menu = $this->sakonnin_builder->messageMenu($factory, $options);
+
+        if ($options['container']->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            $menu['Messages']->addChild('Write PM and send SMS', array('uri' => '#'));
+            $menu['Messages']['Write PM and send SMS']->setLinkAttribute('onclick', 'createPmSmsMessage()');
+            $menu['Messages']->addChild('Write Frontpage message', array('uri' => '#'));
+            $menu['Messages']['Write Frontpage message']->setLinkAttribute('onclick', 'createMessage()');
+
+        }
+
+        // For local customized additions to the main menu.
+        if ($this->custom_builder
+                && method_exists($this->custom_builder, "userMenu"))
+            $menu = $this->custom_builder->userMenu($factory, $options);
         return $menu;
     }
 }
