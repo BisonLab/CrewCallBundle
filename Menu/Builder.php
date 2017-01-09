3ce705741eba58fe848be603840b4facf53fff5a
Thomas Lundquist <github@bisonlab.no>
2017-01-09 20:46:18 +0100

    Restructuring the menu, for now at least.

--- Menu/Builder.php
+++ Menu/Builder.php
@@ -36,13 +36,11 @@ class Builder implements ContainerAwareInterface
 
         if ($this->container->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
             $menu->addChild('Events', array('route' => 'event_index'));
-            $menu->addChild('Listings');
-            $menu['Listings']->addChild('People', array('route' => 'person_index'));
-            $menu['Listings']->addChild('Organizations', array('route' => 'organization_index'));
-            $menu['Listings']->addChild('Location', array('route' => 'location_index'));
-            $menu['Listings']->addChild('Functions', array('route' => 'function_index'));
-
+            $menu->addChild('People', array('route' => 'person_index'));
+            $menu->addChild('Organizations', array('route' => 'organization_index'));
+            $menu->addChild('Locations', array('route' => 'location_index'));
             $menu->addChild('Admin Stuff', array('route' => ''));
+            $menu['Admin Stuff']->addChild('Functions', array('route' => 'function_index'));
             $menu['Admin Stuff']->addChild('Message Types', array('route' => 'messagetype'));
             $menu['Admin Stuff']->addChild('User Admin', array('route' => 'user'));
             $menu['Admin Stuff']->addChild('Report generator', array('route' => 'reports'));
