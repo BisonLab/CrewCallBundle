33e5fab408d2df9b50e8975c40bb7c356ce8cf7e
Thomas Lundquist <github@bisonlab.no>
2017-01-15 20:24:41 +0100

    Gotta have the FOS user model form fiuelds as well of course.

--- Form/PersonType.php
+++ Form/PersonType.php
@@ -36,6 +36,14 @@ class PersonType extends AbstractType
         ));
     }
 
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+
     /**
      * {@inheritdoc}
      */
