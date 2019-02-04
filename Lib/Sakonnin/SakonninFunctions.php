7ee91da648f4ecb1c927b2b25f9b2188bc4e6649
Thomas Lundquist <github@bisonlab.no>
2019-02-04 14:41:54 +0000

    Handling confirm through SMS.

---
+++ Lib/Sakonnin/SakonninFunctions.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace CrewCallBundle\Lib\Sakonnin;
+
+/*
+ */
+
+class SakonninFunctions implements \BisonLab\SakonninBundle\Lib\Sakonnin\SakonninFunctionsInterface
+{
+    protected $container;
+
+    public $callback_functions = array();
+
+    public $forward_functions = array(
+        'smscodehandle' => array(
+            'class' => 'CrewCallBundle\Lib\Sakonnin\SmsHandler',
+            'description' => "Act from code and content in SMS.",
+            'attribute_spec' => null,
+            'needs_attributes' => false,
+        ),
+    );
+
+    public function __construct($container, $options = array())
+    {
+        $this->container = $container;
+    }
+
+    public function getCallbackFunctions()
+    {
+        return $this->callback_functions;
+    }
+
+    public function getForwardFunctions()
+    {
+        return $this->forward_functions;
+    }
+}
