74a5756f0f2de72256004ad16064de3c2212c402
Thomas Lundquist <github@bisonlab.no>
2019-03-17 17:34:12 +0100

    Better and more dashies.

---
+++ Lib/Dashboarder/EventsCalendar.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CrewCallBundle\Lib\Dashboarder;
+
+class EventsCalendar
+{
+    private $router;
+    private $entitymanager;
+    private $twig;
+
+    /*
+     * I may need a lot more here. Too bad I can't use the container.
+     */
+    public function __construct($router, $entitymanager, $twig)
+    {
+        $this->router = $router;
+        $this->entitymanager = $entitymanager;
+        $this->twig = $twig;
+    }
+
+    public function dashize(\CrewCallBundle\Entity\Person $user)
+    {
+        return $this->twig->render('dashboarder/events_calendar.html.twig');
+    }
+}
